3f38b09b56aa26933afd9d227cea01fbe8665e00
Azat Harutyunyan <azat.harutyunyan@eemi.com>
2022-10-18 20:04:53 +0200

    l'heure s'affiche correctement, setInterval marche correctement en consolelog je dois juste l'afficher sur le dom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 
 export interface IHomePageProps {}
 
-function handleClick() {
-  console.log(dayjs().format("MMM, ddd D. h:mm A"));
-}
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
+  // program to display time every 5 seconds
+  function showTime() {
+    // return new date and time
+    let dateTime = new Date();
+
+    // return the time
+    let time = dateTime.toLocaleTimeString();
+
+    console.log(time);
+  }
+
+  let display = setInterval(showTime, 5000);
+
   return (
     <div>
       <p>Voici l'accueil</p>
       <p>
         <Link to="/about">Clique sur la liste !</Link>
       </p>
-      <button onClick={handleClick}>Button</button>
+
+      <p>{dayjs().format(" dddd . h:mm A")}</p>
     </div>
   );
 };
