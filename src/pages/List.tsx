8a5617491ef6af0ca7f50bdb2405c1cf477f9ea6
Azat Harutyunyan <azat.harutyunyan@eemi.com>
2022-10-20 21:28:34 +0200

    bientot finis mais j'ai jamais appris ça

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,28 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import dayjs from "dayjs";
+import React , {useState , useEffect } from 'react'
+import Layout, { Header, Content, Footer } from 'antd/lib/layout/layout'
+import Sider from 'antd/lib/layout/Sider'
+import { Table, Row, Col } from 'antd';
+import axios from 'axios'
 
-function App() {
 
-    const [timer, setTimer] = useState(0);
+
+const columns =[
+    {
+        title:'First Name',
+        dataIndex:'first_name',
+        key:'first_name'
+
+    } ,
+    {
+        title:'Last Name',
+        dataIndex:'last_name',
+        key:'last_name'
+
+    },
+    {
+        title:'email',
+        dataIndex:'email',
+        key:'first_name'
+
+    }
+
+]
+
+
+
+const ListUser = () =>{
+    const [data, setdata] = useState([])
+    const [loading, setloading] = useState(true)
+    
 
     useEffect(() => {
+        getData() }
+    , [])
 
-      setInterval(() => {    
-        setTimer(timer => timer + 5);
-        
-      }, 1000)
+    const getData = async () =>{
+        const res = await axios.get('https://zoo-animal-api.herokuapp.com/animals/rand')
+        setloading(false)
+        setdata(  res.data.data.map(row =>({name:row.first_name,last_name:row.last_name })) );
+                
+    }
 
-    },[] )
+    return (
+        <Layout>
+            <Header>Header 1</Header>
+            
+                <Content  style={{padding:50}}>
+                    <Row>
+                        <Col span={30}/>
+                        <Col span={18}>
+                            <Table dataSource={data} columns={columns} />
+                        </Col>
+                        <Col span={3}/>
+                    </Row>
 
-  return (
-    <div className="App">
-      
 
-        <h1>{timer}  allez..: {dayjs().format("H[h] m[m] s[s]")} </h1>
-
-    </div>
-  );
-  
+                </Content>
+            
+           <Footer>Footer</Footer> 
+        </Layout>
+    )
 }
 
-export default App;
+export default ListUser
